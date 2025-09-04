cf51181c8c80590cb7724c4a28e0e1d0acc08750
Sandro jr <testscodes312@gmail.com>
2025-09-03 21:48:21 -0300

    first commit

---
+++ src/main/java/com/me/quebra_galho_api/QuebraGalhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.me.quebra_galho_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuebraGalhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuebraGalhoApiApplication.class, args);
+	}
+
+}
